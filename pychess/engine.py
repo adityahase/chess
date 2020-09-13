fb2202e10453cc0585c061bce550b0bfae0dbc1b
Aditya Hase <aditya@adityahase.com>
2020-09-13 18:01:47 +0530

    feat: Minimal evaluation system
    Note: Pretty fucking stupid one at that!

--- pychess/engine.py
+++ pychess/engine.py
@@ -1,4 +1,5 @@
 from chess import Board
+from pychess.evaluate import SCORES
 
 
 class Engine:
@@ -13,8 +14,18 @@ class Engine:
                 self.board.push_uci(move)
 
     def go(self):
-        import random
-
         moves = list(self.board.legal_moves)
-        move = random.choice(moves)
+        scores = [(move, self.evaluate(move)) for move in moves]
+        moves = sorted(scores, key=lambda x: x[1], reverse=True)
+        move = moves[0][0]
         return str(move)
+
+    def evaluate(self, move):
+        board = self.board.copy()
+        board.push(move)
+
+        score = 0
+        for square, piece in board.piece_map().items():
+            ii, jj = square // 8, square % 8
+            score += SCORES[piece.piece_type][ii][jj]
+        return score
